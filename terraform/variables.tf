294370cb822323e7a4b8e4e61ef5e775390e0b5e
Ahmed Samir <ahmed.s.abdelrahim7@gmail.com>
2024-02-21 03:54:01 +0200

    adding terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+# Adding the region as a variable, used in provider.tf
+variable "region" {
+  type    = string 
+  default = "us-east-1"
+}
+
+# Adding the ami as a variable, used in main.tf 
+variable "ami" {
+  type    = string 
+  default = "ami-0c7217cdde317cfec"
+}
+
+}
